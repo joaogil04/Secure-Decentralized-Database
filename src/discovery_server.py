fce46a57bc54d39926727cf31f4619c0823f1c11
joaogil04 <joaomariaarauzgil@gmail.com>
2025-12-11 00:46:13 +0000

    Correção de erro no server

--- src/discovery_server.py
+++ src/discovery_server.py
@@ -6,22 +6,25 @@ import time
 # Configuração
 HOST = '0.0.0.0'
 PORT = 5000
-active_peers = {} # Formato: {"peer_id": ("ip", port)}
+active_peers = {} # Formato: {"peer_id": ("ip", port, "pub_key")}
 
 # --- MONITOR DE DISPONIBILIDADE (Slide 47 - Availability) ---
 def monitor_peers():
     """
     Tenta conectar a todos os peers a cada 10s.
     Se um peer não responder, é removido da lista.
-    Isto garante que a lista de peers está sempre atualizada.
     """
     print("[MONITOR] Sistema de Heartbeat iniciado...")
     while True:
         time.sleep(10) 
         peers_to_check = list(active_peers.items()) 
         
-        for peer_id, address in peers_to_check:
-            ip, port = address
+        for peer_id, info in peers_to_check:
+            # CORREÇÃO AQUI: Agora a lista tem 3 coisas (IP, Porta, Key)
+            # Vamos buscar só o IP e Porta para o Ping
+            ip = info[0]
+            port = info[1]
+            
             try:
                 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 s.settimeout(2)
@@ -41,7 +44,7 @@ def monitor_peers():
 def handle_client(conn, addr):
     print(f"[NOVA CONEXÃO] {addr} conectado.")
     try:
-        msg = conn.recv(1024).decode('utf-8')
+        msg = conn.recv(4096).decode('utf-8') # Aumentei o buffer porque a Public Key é grande
         if not msg: return
         request = json.loads(msg)
 
@@ -50,8 +53,12 @@ def handle_client(conn, addr):
         if request['type'] == 'REGISTER':
             peer_id = request['peer_id']
             port = request['port']
-            # Guarda o IP real de onde veio a conexão
-            active_peers[peer_id] = (addr[0], port)
+            # CORREÇÃO AQUI: Ler a Public Key do pedido
+            pub_key = request.get('pub_key', 'CHAVE_NAO_ENVIADA')
+            
+            # CORREÇÃO AQUI: Guardar IP, Porta E Chave Pública (3 elementos)
+            active_peers[peer_id] = (addr[0], port, pub_key)
+            
             print(f"[REGISTO] Peer {peer_id} em {addr[0]}:{port}")
             response = {"status": "SUCCESS", "message": "Registado"}
 
